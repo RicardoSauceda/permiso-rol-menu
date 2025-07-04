87c71cc7ca28d46705efafd67df16733a40a583f
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-07-04 08:16:21 -0600

    Feat: Agregar lógica y vista para el manejo de menús y permisos en el árbol de menús

--- resources/views/rol/permisos.blade.php
+++ resources/views/rol/permisos.blade.php
@@ -5,34 +5,36 @@
 @endpush
 
 @section('content')
-<div class="row" id="menu-tree">
-    <div class="col-xl-12">
-        <div class="card">
-            <div class="card-header border-0">
-                @if ($message = Session::get('success'))
-                <div class="alert alert-success">
-                    <p>{{ $message }}</p>
-                </div>
-                @endif
-                <div class="row align-items-center">
-                    <div class="col-8">
-                        <h3 class="mb-0">Menu</h3>
+<div class="container-fluid mt--6">
+    <div class="row" id="menu-tree">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-header border-0">
+                    @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                    @endif
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">Menu</h3>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <div class="tree-menu">
-                    <h4>Árbol de Menús</h4>
-                    <div class="tree-container">
-                        <ul class="menu-tree">
-                            @if(isset($menuTree) && count($menuTree) > 0)
-                                @foreach($menuTree as $menuItems)
-                                    @include('permiso-rol-menu::menu_items', ['menu' => $menuItems, 'level' => 0])
-                                @endforeach
-                            @else
-                            <li>No hay menús disponibles</li>
-                            @endif
-                        </ul>
+                <div class="card-body">
+                    <div class="tree-menu">
+                        <h4>Árbol de Menús</h4>
+                        <div class="tree-container">
+                            <ul class="menu-tree">
+                                @if(isset($menuTree) && count($menuTree) > 0)
+                                    @foreach($menuTree as $menuItems)
+                                        @include('permiso-rol-menu::permiso_items', ['menu' => $menuItems, 'level' => 0])
+                                    @endforeach
+                                @else
+                                <li>No hay menús disponibles</li>
+                                @endif
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
